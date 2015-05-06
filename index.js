178353a20061455c4e55525226e52ffea146dcee
Feyzo <feyzullahdaldal@gmail.com>
2015-05-06 16:42:52 -0400

    Updated to use latest scxmld

--- index.js
+++ index.js
@@ -1,53 +1,88 @@
 'use strict';
 
 var scxml = require('scxml'),
-  fs = require('fs'),
-  path = require('path'),
   uuid = require('uuid'),
-  rmdir = require('rimraf'),
-  tar = require('tar');
-
+  url = require('url'),
+  http = require('http'),
+  knox = require('knox');
 
 var models = {};
 var instances = {};
-var tmpFolder = 'tmp';
+var instanceSubscriptions = {};
+
+var cephClient = knox.createClient({
+  port: process.env.CEPH_PORT,
+  bucket: process.env.CEPH_BUCKET,
+  endpoint: process.env.CEPH_HOST,
+  key : process.env.CEPH_KEY,
+  secret : process.env.CEPH_SECRET,
+  style: 'path',
+  secure: false
+});
 
 module.exports = function () {
   var server = {};
-  
-  //Delete old temp folder
-  //Create temporary folder for tar streams  
-  rmdir.sync(tmpFolder);
-  fs.mkdir(tmpFolder);
 
-  server.createStatechartWithTar = function (chartName, pack, done) {
-    var statechartFolder = path.join(tmpFolder, chartName);
+  server.createStatechartWithTar = function (chartName, done) {
+    cephClient.getFile(chartName + '/index.scxml', function(err, cephResponse) {
+      var scxmlString = '';
 
-    rmdir(statechartFolder, function (err) {
-      if(err) return done(err);
+      cephResponse.on('data',function(s) {
+        scxmlString += s;
+      });
 
-      fs.mkdir(statechartFolder, function () {
-        var extractor = tar.Extract({path: statechartFolder })
-          .on('error', function (err) { done(err); })
-          .on('end', function () {
-            var mainFilePath = path.resolve(path.join(statechartFolder, 'index.scxml'));
+      cephResponse.on('end',function() {
 
-            scxml.pathToModel(mainFilePath, function (err, model) {
-              models[chartName] = model;
-
-              done(err);
+        scxml.ext.platformModule.platform.getResourceFromUrl = function(fileUrl, cb){
+          console.log('external', fileUrl);
+          var options = url.parse(fileUrl);
+          if(options.hostname){
+            http.get(options, function(res) {
+              var s = '';
+              res.on('data',function(d){
+                s += d;
+              });
+              res.on('end',function(){
+                if(res.statusCode === 200){
+                  cb(null,s);
+                }else{
+                  cb(new Error('HTTP code ' + res.statusCode + ' : ' + s));
+                }
+              });
+            }).on('error', function(e) {
+              cb(e);
             });
-          });
+          }else{
+            //maybe we pass in 
+            cephClient.getFile(chartName + '/' + fileUrl, function(err, res){
+              var s = '';
+              res.on('data',function(d){
+                s += d;
+              });
+              res.on('end',function(){
+                console.log('external result', res.statusCode, s, chartName + '/' + fileUrl);
+                if(res.statusCode === 200){
+                  cb(null,s);
+                }else{
+                  cb(new Error('HTTP code ' + res.statusCode + ' : ' + s));
+                }
+              });
+            });
+          }
+        };
 
-        //Route tar stream to our file system and finalize
-        pack.pipe(extractor);
-        pack.finalize();
+        scxml.documentStringToModel(null, scxmlString, function(err, model) {
+          console.log(model.toString());
+          models[chartName] = model;
+
+          done(err);
+        });
       });
     });
   };
 
   server.createStatechart = function (chartName, scxmlString, done) {
-    scxml.documentStringToModel(null, scxmlString, function (err, model) {
+    scxml.documentStringToModel(null, scxmlString, function(err, model){
       models[chartName] = model;
 
       done(err);
@@ -65,10 +100,11 @@ module.exports = function () {
     done(null, instance.id);
   };
 
-  server.startInstance = function (id, done) {
+  server.startInstance = function (id, sendUrl, done) {
     var instance = instances[id];
 
-    var conf = instance.start();
+    instance.start();
+    var conf = instance.getSnapshot();
 
     done(null, conf);
   };
@@ -79,18 +115,29 @@ module.exports = function () {
     done(null, instance.getSnapshot());
   };
 
-  server.sendEvent = function (id, event, done) {
+  server.sendEvent = function (id, event, sendUrl, done) {
     var instance = instances[id];
 
-    var conf = instance.gen(event);
+    if(event.name === 'system.start') {
+      server.startInstance(id, sendUrl, done);
+    } else {
+      instance.gen(event);
+      var conf = instance.getSnapshot();
 
-    done(null, conf);
+      done(null, conf);
+    }
   };
 
   server.registerListener = function (id, response, done) {
     var instance = instances[id];
 
-    instance.listener = {
+    if(!instance) return done(new Error('Instance not found'));
+
+    instanceSubscriptions[id] = instanceSubscriptions[id] || [];
+
+    instanceSubscriptions[id].push(response);
+
+    var listener = {
       onEntry : function(stateId){
         response.write('event: onEntry\n');
         response.write('data: ' + stateId + '\n\n');
@@ -99,20 +146,44 @@ module.exports = function () {
         response.write('event: onExit\n');
         response.write('data: ' + stateId + '\n\n');
       }
-      //TODO: spec this out
-      // onTransition : function(sourceStateId,targetStatesIds){}
     };
 
-    instance.registerListener(instance.listener);
+    instance.registerListener(listener);
 
     done();
   };
 
-  server.unregisterListener = function (id, done) {
-    var instance = instances[id];
-    
-    if (instance) 
-      instance.unregisterListener(instance.listener);
+  //This is a much needed interface on instance deletion
+  server.unregisterAllListeners = function (id, done) {
+    var subscriptions = instanceSubscriptions[id];
+
+    if(!subscriptions) return done();
+
+    subscriptions.forEach(function (response) {
+      response.end();
+    });
+
+    delete instanceSubscriptions[id];
+
+    if(done) done();
+  };
+
+  server.unregisterListener = function (id, response, done) {
+    //instanceSubscriptions
+    var subscriptions = instanceSubscriptions[id];
+
+    if(!subscriptions) return done();
+    //TODO: somehow remove using response object?
+    //Any unique identifier in response?
+    //http://stackoverflow.com/a/26707009/1744033
+    instanceSubscriptions[id] = subscriptions.filter(function (subResponse) {
+      if(response.uniqueId === subResponse.uniqueId) {
+        response.end();
+        return false;
+      }
+
+      return true;
+    });
 
     if(done) done();
   };
